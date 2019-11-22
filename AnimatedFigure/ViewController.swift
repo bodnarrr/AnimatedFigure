8436c2354d3165aae522dc95673a26446e7a4ab9
Andrii Bodnar <a.bodnarrr@gmail.com>
2019-11-22 22:24:56 +0200

    Initial commit

---
+++ AnimatedFigure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedFigure
+//
+//  Created by Andrii Bodnar on 11/22/19.
+//  Copyright © 2019 Andrii Bodnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
